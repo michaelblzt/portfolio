89d3e9dc4f79f12f71f07140124d3e26c9a3e736
“michaelblzt” <michael.blzt@gmail.com>
2020-11-26 12:19:56 +0100

    gtag

--- dist/nprogress.9707b974.js
+++ dist/nprogress.9707b974.js
@@ -218,7 +218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58241" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61086" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
